9fc05a2d426053afd81965f61d15c354b5de3d5c
Randy <o1984531@gmail.com>
2019-09-26 00:19:36 +0800

    [Fix] Fix the addView of contain to add duplicate list

--- app/src/main/java/com/ktpractice/flow/main/TeamPagerAdapter.kt
+++ app/src/main/java/com/ktpractice/flow/main/TeamPagerAdapter.kt
@@ -7,6 +7,8 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.LinearLayout
 import androidx.core.content.ContextCompat
+import androidx.core.view.contains
+import androidx.paging.PagedList
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import androidx.viewpager.widget.PagerAdapter
@@ -15,30 +17,33 @@ import com.ktpractice.model.Person
 
 class TeamPagerAdapter(val mCtx: Context, val mTabTeamNameArray: Array<String>) : PagerAdapter() {
 
-    private var mAdapterMap: LinkedHashMap<String, PersonListAdapter> = LinkedHashMap()
     private var mRvList: ArrayList<RecyclerView> = ArrayList()
 
-    override fun instantiateItem(container: ViewGroup, position: Int): Any {
-        val rvList = RecyclerView(mCtx)
-        val layoutMgr = LinearLayoutManager(mCtx)
-        layoutMgr.orientation = RecyclerView.VERTICAL
-        rvList.layoutManager = layoutMgr
+    init {
+        for(i in mTabTeamNameArray.indices) {
+            val rvList = RecyclerView(mCtx)
+            val layoutMgr = LinearLayoutManager(mCtx)
+            layoutMgr.orientation = RecyclerView.VERTICAL
+            rvList.layoutManager = layoutMgr
 
-        rvList.setBackgroundColor(ContextCompat.getColor(mCtx, R.color.black_1))
-        rvList.setHasFixedSize(true)
-        container.addView(rvList)
-        mRvList.add(rvList)
-        if (position <= mAdapterMap.size - 1) {
-            rvList.adapter = mAdapterMap.let {
-                it.values.toTypedArray()[position]
-            }
+            rvList.setBackgroundColor(ContextCompat.getColor(mCtx, R.color.black_1))
+            rvList.setHasFixedSize(true)
+            mRvList.add(rvList)
+        }
+    }
+
+    override fun instantiateItem(container: ViewGroup, position: Int): Any {
+        val rvList = mRvList[position]
+
+        if(!container.contains(rvList)) {
+            // Avoid add the list duplicate
+            container.addView(rvList)
         }
         return rvList
     }
 
     override fun destroyItem(container: ViewGroup, position: Int, `object`: Any) {
         container.removeView(`object` as View)
-        mRvList.remove(`object`)
     }
 
     override fun isViewFromObject(view: View, `object`: Any): Boolean {
@@ -53,15 +58,13 @@ class TeamPagerAdapter(val mCtx: Context, val mTabTeamNameArray: Array<String>) 
         return mTabTeamNameArray.get(position)
     }
 
-    fun addContentList(team: String, personList: List<Person>?) {
+    fun addContentList(team: String, personList: PagedList<Person>?) {
         val adapter = PersonListAdapter(mCtx, personList)
-        mAdapterMap.put(team, adapter)
 
-        if (mAdapterMap.size > mRvList.size) return
-        val index = mAdapterMap.let {
+        val index = mTabTeamNameArray.let {
             var index = -1
 
-            for (key in mAdapterMap.keys) {
+            for (key in mTabTeamNameArray) {
                 ++index
                 if (TextUtils.equals(key, team)) break
             }
