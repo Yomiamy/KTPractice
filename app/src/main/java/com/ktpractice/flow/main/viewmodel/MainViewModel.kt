d651fbf190ad6836c6e495503bfb09eb936e5e47
Randy <o1984531@gmail.com>
2023-02-19 00:52:40 +0800

    [modify] Wrap the ui state updating in viewModelScope

--- app/src/main/java/com/ktpractice/flow/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/ktpractice/flow/main/viewmodel/MainViewModel.kt
@@ -3,6 +3,8 @@ package com.ktpractice.flow.main.viewmodel
 import android.content.Context
 import androidx.lifecycle.*
 import com.ktpractice.flow.main.repository.MainRepository
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainViewModel<T>(val context: T): ViewModel() where T:Context, T: LifecycleOwner {
 
@@ -11,9 +13,11 @@ class MainViewModel<T>(val context: T): ViewModel() where T:Context, T: Lifecycl
     private var mRepository: MainRepository = MainRepository(context)
 
     fun loadPageByTeamName(teamName: String) {
-        mRepository.getPersonList(teamName).observe(context, Observer {
-            mainPageUiState.value = MainPageUiState(teamName, it)
-        })
+        mRepository.getPersonList(teamName).observe(context) {
+            viewModelScope.launch {
+                mainPageUiState.value = MainPageUiState(teamName, it)
+            }
+        }
     }
 
     fun calculateNextPage(curListItemCount: Int) {
