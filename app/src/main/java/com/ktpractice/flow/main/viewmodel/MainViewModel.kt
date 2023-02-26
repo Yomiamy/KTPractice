955a8fb63f368241cfce128cee4864025de3e56c
Randy <o1984531@gmail.com>
2023-02-26 19:07:49 +0800

    [modify] Change the exposed LiveData by read-only LiveData

--- app/src/main/java/com/ktpractice/flow/main/viewmodel/MainViewModel.kt
+++ app/src/main/java/com/ktpractice/flow/main/viewmodel/MainViewModel.kt
@@ -3,19 +3,20 @@ package com.ktpractice.flow.main.viewmodel
 import android.content.Context
 import androidx.lifecycle.*
 import com.ktpractice.flow.main.repository.MainRepository
-import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 
 class MainViewModel<T>(val context: T): ViewModel() where T:Context, T: LifecycleOwner {
 
-    var mainPageUiState: MutableLiveData<MainPageUiState?> = MutableLiveData()
 
-    private var mRepository: MainRepository = MainRepository(context)
+    private val mMainPageUiState: MutableLiveData<MainPageUiState?> = MutableLiveData()
+    private val mRepository: MainRepository = MainRepository(context)
+
+    val mainPageUiState:LiveData<MainPageUiState?> = mMainPageUiState
 
     fun loadPageByTeamName(teamName: String) {
         mRepository.getPersonList(teamName).observe(context) {
             viewModelScope.launch {
-                mainPageUiState.value = MainPageUiState(teamName, it)
+                mMainPageUiState.value = MainPageUiState(teamName, it)
             }
         }
     }
