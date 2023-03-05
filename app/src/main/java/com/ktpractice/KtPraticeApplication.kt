dbc58f8aca72fff783ee010eca79e1b8f10344a9
Randy <o1984531@gmail.com>
2023-03-06 00:29:47 +0800

    [modify] Config the Application annotated by HiltAndroidApp

--- app/src/main/java/com/ktpractice/KtPraticeApplication.kt
+++ app/src/main/java/com/ktpractice/KtPraticeApplication.kt
@@ -3,7 +3,9 @@ package com.ktpractice
 import android.app.Application
 import android.content.Context
 import androidx.multidex.MultiDex
+import dagger.hilt.android.HiltAndroidApp
 
+@HiltAndroidApp
 class KtPraticeApplication:Application() {
 
     override fun onCreate() {
