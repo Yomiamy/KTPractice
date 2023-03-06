6c94c27b562bd50c844134851b215f9dad385762
Randy <o1984531@gmail.com>
2023-03-07 00:08:42 +0800

    [add] Add the singleton annotation

--- app/src/main/java/com/ktpractice/di/AppModule.kt
+++ app/src/main/java/com/ktpractice/di/AppModule.kt
@@ -8,13 +8,16 @@ import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.android.qualifiers.ApplicationContext
 import dagger.hilt.components.SingletonComponent
+import javax.inject.Singleton
 
 @Module
 @InstallIn(SingletonComponent::class)
 object AppModule {
+    @Singleton
     @Provides
     fun provideContext(@ApplicationContext context: Context): Context = context
 
+    @Singleton
     @Provides
     fun provideMainRepository(context: Context): IMainRepository = MainRepository(context)
 }
