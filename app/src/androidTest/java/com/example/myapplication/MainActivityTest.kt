23f002523c4bd5f70649d7a19e5fda84825f47e3
Randy <o1984531@gmail.com>
2023-05-05 00:04:05 +0800

    [add] Add tab click and scroll action

--- app/src/androidTest/java/com/example/myapplication/MainActivityTest.kt
+++ app/src/androidTest/java/com/example/myapplication/MainActivityTest.kt
@@ -1,16 +1,19 @@
 package com.example.myapplication
 
 
+import androidx.recyclerview.widget.RecyclerView
 import androidx.test.espresso.Espresso.onView
 import androidx.test.espresso.action.ViewActions
 import androidx.test.espresso.assertion.ViewAssertions.matches
+import androidx.test.espresso.matcher.ViewMatchers
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.ext.junit.rules.ActivityScenarioRule
 import com.ktpractice.flow.main.view.MainActivity
 import org.junit.Rule
 import org.junit.Test
 import com.ktpractice.R
-import java.util.EnumSet.allOf
+import org.hamcrest.CoreMatchers.allOf
+import org.hamcrest.core.Is.`is`
 
 
 class MainActivityTest {
@@ -20,8 +23,16 @@ class MainActivityTest {
 
 
     @Test
-    fun testShowMainActivity() {
+    fun testTabClick() {
         onView(withText("Dynamo")).perform(ViewActions.click())
-        isCompletelyDisplayed()
+
+        onView(withText("Elastic")).perform(ViewActions.click())
+
+        onView(withText("Rangers")).perform(ViewActions.click())
+    }
+
+    @Test
+    fun scrollRecycleView() {
+        onView(allOf(withParent(withId(R.id.vp_view_pager)), withClassName(`is`("androidx.recyclerview.widget.RecyclerView")))).perform(ViewActions.swipeUp())
     }
 }
